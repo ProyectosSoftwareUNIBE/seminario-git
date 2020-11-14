2d188599724aea315c3211af0b98750811b99aad
rafael <rfalconi@unibe.edu.ec>
2020-11-14 09:30:22 -0500

    angular route parte 1

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-details',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  public id: String;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+ 
   }
 
 }
